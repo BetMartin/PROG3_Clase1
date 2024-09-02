84e8aeced4e75079ffc566de9e723f966af00ef7
betsa <betsabemartin@gmail.com>
2024-09-02 17:31:17 -0300

    ParteB Ejercicio2

--- src/main/java/EjercicioB/Empleado.java
+++ src/main/java/EjercicioB/Empleado.java
@@ -1,4 +1,132 @@
 package EjercicioB;
+import java.util.ArrayList;
+import java.util.Calendar;
 
 public class Empleado {
+    public String nombre;
+    public long cuit;
+    public String domicilio;
+    public String email;
+    public ArrayList<Asistencia> asistencia;
+    public ArrayList<Tardanza> tardanzas;
+    public RegimenHorario regimen;
+
+    public Empleado(String nombre, long cuit, String domicilio, ArrayList<Asistencia> asistencia, String email, ArrayList<Tardanza> tardanzas, RegimenHorario regimen) {
+        this.nombre = nombre;
+        this.cuit = cuit;
+        this.domicilio = domicilio;
+        this.asistencia = asistencia;
+        this.email = email;
+        this.tardanzas = tardanzas;
+        this.regimen = regimen;
+    }
+
+public Empleado(){}
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public long getCuit() {
+        return cuit;
+    }
+
+    public void setCuit(long cuit) {
+        this.cuit = cuit;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public ArrayList<Asistencia> getAsistencia() {
+        return asistencia;
+    }
+
+    public void setAsistencia(ArrayList<Asistencia> asistencia) {
+        this.asistencia = asistencia;
+    }
+
+    public ArrayList<Tardanza> getTardanzas() {
+        return tardanzas;
+    }
+
+    public void setTardanzas(ArrayList<Tardanza> tardanzas) {
+        this.tardanzas = tardanzas;
+    }
+
+    public RegimenHorario getRegimen() {
+        return regimen;
+    }
+
+    public void setRegimen(RegimenHorario regimen) {
+        this.regimen = regimen;
+    }
+
+
+    public ArrayList<Asistencia> getAsistenciaXMesXAnio(int mes, int anio) {
+        ArrayList<Asistencia> asistenciasFiltradas = new ArrayList<>();
+
+        for (Asistencia asistencia : this.asistencia) {
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(asistencia.getFecha());
+            int mesAsistencia = cal.get(Calendar.MONTH) + 1; // Los meses en Calendar comienzan en 0
+            int anioAsistencia = cal.get(Calendar.YEAR);
+
+            if (mesAsistencia == mes && anioAsistencia == anio) {
+                asistenciasFiltradas.add(asistencia);
+            }
+        }
+
+        return asistenciasFiltradas;
+    }
+    public ArrayList<Tardanza> getDiasConTardanza(int mes, int anio) {
+        ArrayList<Tardanza> tardanzas = new ArrayList<>();
+        ArrayList<Asistencia> asistencias = getAsistenciaXMesXAnio(mes, anio);
+
+        for (Asistencia asistencia : asistencias) {
+            RegimenHorario regimen = this.getRegimen();
+            int horaIngreso = regimen.getHoraIngreso();
+            int minutoIngreso = regimen.getMinutoIngreso();
+            int totalMinutosRegimen = horaIngreso * 60 + minutoIngreso;
+
+            int horaAsistencia = asistencia.getHora();
+            int minutoAsistencia = asistencia.getMinuto();
+            int totalMinutosAsistencia = horaAsistencia * 60 + minutoAsistencia;
+
+            // Verificar si la asistencia supera en más de 15 minutos el horario establecido
+
+            if (totalMinutosAsistencia > totalMinutosRegimen + 15) {
+                Tardanza tardanza = new Tardanza(
+                        asistencia.getId(),
+                        "Tardanza",
+                        asistencia.getFecha(),
+                        asistencia.getHora(),
+                        asistencia.getMinuto(),
+                        this
+                );
+                tardanzas.add(tardanza);
+            }
+        }
+
+        return tardanzas;
+    }
+
 }
--- src/main/java/EjercicioB/Tardanza.java
+++ src/main/java/EjercicioB/Tardanza.java
@@ -1,4 +1,77 @@
 package EjercicioB;
-
+import java.util.Date;
 public class Tardanza {
+    public long id;
+    public String tipo;
+    public Date fecha;
+    public int hora;
+    public int minuto;
+    public Empleado empleado;
+
+
+    public Tardanza(long id, String tipo, Date fecha, int hora, int minuto, Empleado empleado) {
+        this.id = id;
+        this.tipo = tipo;
+        this.fecha = fecha;
+        this.hora = hora;
+        this.minuto = minuto;
+        this.empleado = empleado;
+    }
+ public Tardanza(){}
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getHora() {
+        return hora;
+    }
+
+    public void setHora(int hora) {
+        this.hora = hora;
+    }
+
+    public int getMinuto() {
+        return minuto;
+    }
+
+    public void setMinuto(int minuto) {
+        this.minuto = minuto;
+    }
+
+    public Empleado getEmpleado() {
+        return empleado;
+    }
+
+    public void setEmpleado(Empleado empleado) {
+        this.empleado = empleado;
+    }
+
+
+
+
+
+
+
 }
